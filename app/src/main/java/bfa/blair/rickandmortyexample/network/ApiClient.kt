e24daf64c3d23fe2006fd387be863869a9786ebe
blairMe <felixanachoni@gmail.com>
2022-11-22 23:31:24 +0300

    Rick and Morty

--- app/src/main/java/bfa/blair/rickandmortyexample/network/ApiClient.kt
+++ app/src/main/java/bfa/blair/rickandmortyexample/network/ApiClient.kt
@@ -2,8 +2,12 @@ package bfa.blair.rickandmortyexample.network
 
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 object ApiClient {
     // https://rickandmortyapi.com/api/character/?page=1
@@ -16,4 +20,15 @@ object ApiClient {
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
+
+    val apiService : ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+
+}
+
+// Api Service
+interface ApiService {
+    @GET("character")
+    fun fetchCharacters(@Query("page") page : String) : Call<CharacterResponse>
 }
